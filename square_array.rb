da20513f81797d63fca8674f359920b141ace678
Johnny Soong <johnnysoong@hotmail.com>
2018-07-31 12:51:10 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -2,8 +2,17 @@ def square_array(array)
   # your code here
    arr = []
   array.each do |element|
+<<<<<<< HEAD
     arr.push(element**2)
   end
   arr
 end
 
+=======
+    arr.push(element ** 2)
+    arr
+  end
+end
+
+puts square_array([2,3,4,5])
+>>>>>>> e08f3eba894f1a1828c6cf766617aeabfc79489f
